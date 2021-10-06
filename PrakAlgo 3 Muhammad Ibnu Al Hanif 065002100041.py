03aed20eae5f8bc4d95b4c53303cca757cca1ecc
muhammmadibnualhanif <91421315+muhammmadibnualhanif@users.noreply.github.com>
2021-10-06 09:26:14 +0700

    Add files via upload

---
+++ PrakAlgo 3 Muhammad Ibnu Al Hanif 065002100041.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  6 08:16:39 2021
+
+@author: USER
+"""
+  
+print ("@  @    @    @   @   @   @@@")
+print ("@  @   @ @   @@  @   @   @  ")
+print ("@@@@  @@@@@  @ @ @   @   @@ ")
+print ("@  @ @     @ @   @   @   @  ")
+
+pertama = int(input('Masukkan angka awal : '))
+terakhir = int(input('Masukkan angka akhir : '))
+
+while pertama<=terakhir:
+    print (pertama, terakhir)
+    pertama = pertama +1
+    terakhir = terakhir -1
