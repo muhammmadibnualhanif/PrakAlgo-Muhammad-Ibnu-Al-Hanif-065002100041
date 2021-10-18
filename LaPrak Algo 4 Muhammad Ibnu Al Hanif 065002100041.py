dce7316bccffc40d7c8a7eef51f6bd8023048532
muhammmadibnualhanif <91421315+muhammmadibnualhanif@users.noreply.github.com>
2021-10-18 23:42:41 +0700

    Add files via upload

---
+++ LaPrak Algo 4 Muhammad Ibnu Al Hanif 065002100041.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 18 22:45:47 2021
+
+@author: hanif
+"""
+
+a = ("@  @    @    @   @   @   @@@")
+b = ("@  @   @ @   @@  @   @   @  ")
+c = ("@@@@  @@@@@  @ @ @   @   @@ ")
+d = ("@  @ @     @ @   @   @   @  ")
+
+hanif=0
+while hanif==0:
+    a=input('masukan list angka: ')
+    genap=[]
+    ganjil=[]
+    for a in a.split():
+
+        if int(a)%2==0:
+                print('terdapat bilangan genap')
+                break
+        if int(a)%2==1:
+            
+            genap.append(int(a))
+            print('terdapat bilangan genap')
+            break
+
+        else:
+            print('tidak terdapat bilangan genap')
