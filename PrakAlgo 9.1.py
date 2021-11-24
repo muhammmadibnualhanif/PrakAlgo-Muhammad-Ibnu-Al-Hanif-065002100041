1a2f1057b5c2647d2eb66b3ffb4550f9a36075d9
muhammmadibnualhanif <91421315+muhammmadibnualhanif@users.noreply.github.com>
2021-11-24 13:10:43 +0700

    Add files via upload

---
+++ PrakAlgo 9.1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 24 08:46:48 2021
+
+@author: hanif
+"""
+
+A=[1, 2, 3, 4, 5]
+B=tuple(A)
+print("List :",A)
+print("Hasil konversi list menjadi Tuple :",B)
