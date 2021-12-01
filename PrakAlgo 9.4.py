3cc8e7dae081ec819ffef0828654a0824be1ecf4
muhammmadibnualhanif <91421315+muhammmadibnualhanif@users.noreply.github.com>
2021-12-01 11:45:44 +0700

    Add files via upload

---
+++ PrakAlgo 9.4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 10:04:17 2021
+
+@author: hanif
+"""
+
+print(["z","xvc","cac",244])
+print('- cac')
+print('-244')
+print('Terdapat 2 string yang memenuhi kriteria')
